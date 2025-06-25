3998885e298521aa15e2f3e43cf8511873f496cd
Stavros Chairetas <79275132+Stavrosch@users.noreply.github.com>
2025-06-25 18:42:54 +0300

    asteroidwithctk

--- GUIs/asteroid_detection_window.py
+++ GUIs/asteroid_detection_window.py
@@ -4,14 +4,15 @@ import Utilities as ut
 from astropy.visualization.mpl_normalize import ImageNormalize
 import os
 from tkinter import ttk
+import customtkinter as ctk
 
 def show_progress_and_run(parent_window,pltfrm,image_path,threshold):
 
-    progs_win=tk.Toplevel(parent_window)
+    progs_win=ctk.CTkToplevel(parent_window)
     progs_win.title("Processing...")
     progs_win.geometry("300x100")
 
-    progs_lbl=tk.Label(progs_win,text="Running detection...")
+    progs_lbl=ctk.CTkLabel(progs_win,text="Running detection...")
     progs_lbl.pack(pady=10)
 
     progs_bar=ttk.Progressbar(progs_win,orient="horizontal",length=200,mode="determinate")
@@ -27,46 +28,47 @@ def choose_file(window,threshold,pltfrm):
     )
     if filepth:
         filenm=os.path.basename(filepth)
-        filelbl.config(text=f"Selected File: {filenm}",fg="green")
-        start.config(state=tk.NORMAL)
+        filelbl.configure(text=f"Selected File: {filenm}",text_color="green")
+        start.configure(state=ctk.NORMAL)
 
     else:
-        filelbl.config(text="No file selected",fg="red")
-        start.config(state=tk.DISABLED)
+        filelbl.configure(text="No file selected",text_color="red")
+        start.configure(state=ctk.DISABLED)
 
 def create_window(parent):
-    winast=tk.Toplevel(parent)
+    winast=ctk.CTkToplevel(parent)
     winast.title("Asteroid Detection Window")
     winast.geometry("1000x800")
 
-    filefrm=tk.Frame(winast)
+    filefrm=ctk.CTkFrame(winast)
     filefrm.pack(pady=10)
-    selectlbl=tk.Label(filefrm,text="Select a FITS file for analysis")
-    selectlbl.pack(side=tk.LEFT,padx=5)
+    selectlbl=ctk.CTkLabel(filefrm,text="Select a FITS file for analysis")
+    selectlbl.pack(side=ctk.LEFT,padx=5)
 
-    buton1=tk.Button(filefrm,text="Choose File",command=lambda: choose_file(winast,magsldr.get(),pltfrm))
-    buton1.pack(side=tk.LEFT,padx=5)
+    buton1=ctk.CTkButton(filefrm,text="Choose File",command=lambda: choose_file(winast,magsldr.get(),pltfrm))
+    buton1.pack(side=ctk.LEFT,padx=5)
 
     global filelbl
-    filelbl=tk.Label(winast,text="No file selected",fg="blue")
+    filelbl=ctk.CTkLabel(winast,text="No file selected",text_color="blue")
     filelbl.pack(pady=2)
 
-    sldrfrm=tk.Frame(winast)
+    sldrfrm=ctk.CTkFrame(winast)
     sldrfrm.pack(pady=2)
 
-    maglb=tk.Label(sldrfrm,text="Select Magnitude Threshold (for filtering stars):")
-    maglb.pack(side=tk.LEFT,padx=5)
+    global magsldr
+    maglb=ctk.CTkLabel(sldrfrm,text="Select Magnitude Threshold (for filtering stars):")
+    maglb.pack(side=ctk.LEFT,padx=5)
 
-    magsldr=tk.Scale(sldrfrm,from_=0.002,to=0.04,orient=tk.HORIZONTAL,length=100,resolution=0.002)
+    magsldr=ctk.CTkSlider(sldrfrm,from_=0.002,to=0.04,number_of_steps=19)
     magsldr.set(0.01)
-    magsldr.pack(side=tk.LEFT,padx=5)
+    magsldr.pack(side=ctk.LEFT,padx=5)
 
     global start
-    start=tk.Button(winast,text="Run Detection",state=tk.DISABLED,command=lambda: show_progress_and_run(winast,pltfrm,filepth,magsldr.get()))
+    start=ctk.CTkButton(winast,text="Run Detection",state=ctk.DISABLED,command=lambda: show_progress_and_run(winast,pltfrm,filepth,magsldr.get()))
     start.pack(anchor='n',pady=2)
 
-    plt_lbl=tk.Label(winast,text=f"Asteroid Detection",font=("Helvetica",18))
+    plt_lbl=ctk.CTkLabel(winast,text=f"Asteroid Detection",font=("Helvetica",18))
     plt_lbl.pack(anchor='n',pady=0)
 
-    pltfrm=tk.Frame(winast)
-    pltfrm.pack(fill=tk.BOTH,expand=True)
+    pltfrm=ctk.CTkFrame(winast)
+    pltfrm.pack(fill=ctk.BOTH,expand=True)
